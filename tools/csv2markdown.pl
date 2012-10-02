b39a134146344647bc63a98c8a5db49252d7a397
Salve J. Nilsen <sjn+gnurf@kaizendo.org>
2012-10-02 20:51:14 +0200

    Update contact info; Better variable names; etc.
    - Simplify usage
    - Fix bug in $csv_content assignment
    - Simplify template keywords and markdown URL substitution
    - Rename output_file to generate_mkd_file_from
    - Also, run Perl::Tidy

--- tools/csv2markdown.pl
+++ tools/csv2markdown.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 # 20120927/JT
-# Author: oslopolitimoro <konakt@oslopolitimoro.org>
+# Author: oslopolitimoro <kontakt@oslopolitimoro.org>
 # Takes a csv as input file and puts out every tupel as markdown file
 #   ready for octopress to deploy
 #
@@ -14,67 +14,49 @@
 #	STDOUT: None, creates markdown files in the current folder though.
 #
 # ------------------------------------------------------------------------
-use strict;
-use warnings;
-use Getopt::Long;    # Handling parameters.
-use Text::CSV;       # CSV file handling.
-use Text::Iconv;     # Converting Strings for special characters.
-use File::Slurp;
-use utf8;
-
 use v5.10.1;
 
+use strict;
+use warnings;
+use utf8;
+use Getopt::Long;    # Handling parameters.
+use Text::CSV;       # CSV file handling.
+use File::Slurp;
 
+# Fetch filename from command line
+die usage() unless @ARGV;
 Getopt::Long::Configure('bundling');
-
-die usage("Error: not enough parameters specified. Aborting.")
-  unless @ARGV;
-
-# Get parameters
-GetOptions( 'i|inputfile=s' => \my $pathinputfile, );
+GetOptions( 'i|inputfile=s' => \my $inputfile );
 
 # Read CSV File
-my $csv_content = read_file($pathinputfile, { binmode => ':utf8' })
-  || die usage("Error: Can't read csv file: '$!'. Aborting.");
-
 my $csv = Text::CSV->new( { sep_char => ',', quote_char => '"' } );
-my @content = split /\n/, $csv_content;
-while ( my $line = shift @content ) {
-    next if $line =~ m/^#/;
-    if ( ! $csv->parse($line) ) {
-       warn "Failed to parse line: " . $csv->error_input;
-       next;
+my @content = read_file( $inputfile, { binmode => ':utf8' } );
+
+foreach (@content) {
+    next if m/^#/;
+    if ( !$csv->parse($_) ) {
+        warn "Failed to parse line: " . $csv->error_input;
+        next;
     }
-    my @fields_in_tweet = $csv->fields();
-    output_file(@fields_in_tweet);
+    generate_mkd_file_from( $csv->fields() );
 }
 
+sub usage {"Syntax:\n $0 -i|--inputfile <input.csv>\n"}
 
-sub usage {          # Routine prints the Syntax
-    my $error_msg = shift // "";
-    my $usage = "Syntax:
-	$0: -i|inputfile <input.csv>\n";
+sub generate_mkd_file_from {
+    my ( $date, $link, $title, $text ) = @_;
 
-    $error_msg = "\nError: $error_msg\n\n" if $error_msg;
-
-    return $usage . $error_msg;
-}
-
-
-sub output_file {
-    my( $date, $link, $title, $text ) = @_;
-
-    my $content = read_file(\*DATA);  # Get template from __DATA__
-    $content =~ s/\<tweettitle\>/$title/ig;
-    $content =~ s/\<timestamp\>/$date/ig;
-    $content =~ s/\<tweettext\>/$text/ig;
-    $content =~ s/\<tweetlink\>/$link/ig;
+    my $content = read_file( \*DATA );    # Get template from __DATA__
+    $content =~ s/\<title\>/$title/ig;
+    $content =~ s/\<time\>/$date/ig;
+    $content =~ s/\<text\>/$text/ig;
+    $content =~ s/\<link\>/$link/ig;
 
     # Make markdownlinks of the text if available
-    $text =~ s/\s(http\:\/\/[^\s]+)/ \[$1\]\($1\)/;
+    $text =~ s|\s(http://[^\s]+)| \[$1\]($1)|;
 
     # Build up filename and remove everything that doesn't belong there.
-    my $filename = $date . "-" . lc( $title );
+    my $filename = $date . "-" . lc($title);
     $filename =~ s/\s\d{1,2}:\d{2}//;
     $filename =~ s/å/aa/gi;
     $filename =~ s/ø/oe/gi;
@@ -84,17 +66,16 @@ sub output_file {
     $filename =~ s/\_+$//ig;
     $filename .= ".mkd";
 
-    write_file( $filename, { binmode => ':utf8'}, $content );
+    write_file( $filename, { binmode => ':utf8' }, $content );
 }
-
 
 __DATA__
 ---
 layout: post
-title: "<tweettitle>"
-date: <timestamp>
+title: "<title>"
+date: <time>
 comments: true
 categories: 
 ---
-> <tweettext>
-- [Operasjonssentralen](<tweetlink>)
+> <text>
+- [Operasjonssentralen](<link>)
